72dc67d0133d9d4db2df6580c8acb5e51c445e74
FeliIsra <43824131+FeliIsra@users.noreply.github.com>
2019-08-03 03:24:23 -0300

    Update PruebaBase.sh

--- SH/PruebaBase.sh
+++ SH/PruebaBase.sh
@@ -12,5 +12,5 @@ mkdir -p Tablas
 mkdir -p Metadata
 cd /home/utnso/tp-2019-1c-UbUTNu
 cd Pruebas/ConfigPruebaBase/
-cp -a Metadata.bin ../../lfs-base/Metadata
+cp -a Metadata.bin /home/utnso/lfs-base/Metadata
 exit
