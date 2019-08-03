a151fdaab9044fd3ce5e15e0e261522ab7a0e877
lucascheistwer <38320116+lucascheistwer@users.noreply.github.com>
2019-08-03 03:14:21 -0300

    Update PruebaMemoria.sh

--- SH/PruebaMemoria.sh
+++ SH/PruebaMemoria.sh
@@ -3,13 +3,13 @@ cd ../Pruebas/ConfigPruebaMemoria/
 cp -a 1 ../../Operativos/Memoria/
 cp -a Kernel.config ../../Operativos/
 cp -a LFS.config ../../Operativos/
-cd ../../
+cd /home/utnso
 mkdir -p lfs-prueba-memoria
 cd lfs-prueba-memoria
 mkdir -p Bloques
 mkdir -p Tablas
 mkdir -p Metadata
-cd ..
+cd /home/utno/tp-2019-1c-UbUTNu
 cd Pruebas/ConfigPruebaMemoria/
-cp -a Metadata.bin ../../lfs-prueba-memoriae/Metadata
+cp -a Metadata.bin ../../lfs-prueba-memoria/Metadata
 exit
