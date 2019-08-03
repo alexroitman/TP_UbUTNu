ee69857609d0a749ad29a51de7b69e9af755eca9
lucascheistwer <38320116+lucascheistwer@users.noreply.github.com>
2019-08-03 03:13:23 -0300

    Update PruebaLFS.sh

--- SH/PruebaLFS.sh
+++ SH/PruebaLFS.sh
@@ -5,13 +5,13 @@ cp -a 2 ../../Operativos/Memoria/
 cp -a 3 ../../Operativos/Memoria/
 cp -a Kernel.config ../../Operativos/
 cp -a LFS.config ../../Operativos/
-cd ../../
+cd /home/utnso
 mkdir -p lfs-compactacion
 cd lfs-compactacion
 mkdir -p Bloques
 mkdir -p Tablas
 mkdir -p Metadata
-cd ..
+cd /home/utno/tp-2019-1c-UbUTNu
 cd Pruebas/ConfigPruebaLFS/
 cp -a Metadata.bin ../../lfs-compactacion/Metadata
 exit
