7797f5ec9134816f1e8ebee0c49e682f7859c811
lucascheistwer <38320116+lucascheistwer@users.noreply.github.com>
2019-08-03 03:14:55 -0300

    Update PruebaStress.sh

--- SH/PruebaStress.sh
+++ SH/PruebaStress.sh
@@ -7,13 +7,13 @@ cp -a 4 ../../Operativos/Memoria/
 cp -a 5 ../../Operativos/Memoria/
 cp -a Kernel.config ../../Operativos/
 cp -a LFS.config ../../Operativos/
-cd ../../
+cd /home/utnso
 mkdir -p lfs-stress
 cd lfs-stress
 mkdir -p Bloques
 mkdir -p Tablas
 mkdir -p Metadata
-cd ..
+cd /home/utno/tp-2019-1c-UbUTNu
 cd Pruebas/ConfigPruebaStress/
 cp -a Metadata.bin ../../lfs-stress/Metadata
 exit
