ac2ee650fe34704ad3aaf9ba66d82deec0bcbff0
lucascheistwer <38320116+lucascheistwer@users.noreply.github.com>
2019-08-03 03:11:24 -0300

    Update PruebaBase.sh

--- SH/PruebaBase.sh
+++ SH/PruebaBase.sh
@@ -4,13 +4,13 @@ cp -a 1 ../../Operativos/Memoria/
 cp -a 2 ../../Operativos/Memoria/
 cp -a Kernel.config ../../Operativos/
 cp -a LFS.config ../../Operativos/
-cd ../../
+cd /home/utnso
 mkdir -p lfs-base
 cd lfs-base
 mkdir -p Bloques
 mkdir -p Tablas
 mkdir -p Metadata
-cd ..
+cd /home/utno/tp-2019-1c-UbUTNu
 cd Pruebas/ConfigPruebaBase/
 cp -a Metadata.bin ../../lfs-base/Metadata
 exit
